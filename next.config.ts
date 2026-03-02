d960fa30c89937be7130228251cb092eb22f810b
nirajjoshi1 <infocodewithniraj@gmail.com>
2026-03-02 21:19:59 +0545

    08-voice-managment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators:false,
+  experimental: {
+    proxyClientMaxBodySize:"20mb",
+  },
 };
 
 export default withSentryConfig(nextConfig, {
