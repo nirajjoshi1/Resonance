347e348b90770e9874fa320f2c49a293d6e43a03
nirajjoshi1 <infocodewithniraj@gmail.com>
2026-03-02 22:19:30 +0545

    finilize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCReactProvider } from "@/trpc/client";
-import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -23,7 +24,6 @@ export const metadata: Metadata = {
   description: "AI-powered text-to-speech and voice cloning platform",
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -32,12 +32,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <TRPCReactProvider>
-      <html lang="en">
-        <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
-          {children}
-          <Toaster />
-        </body>
-      </html>
+        <html lang="en">
+          <body
+            className={`${inter.variable} ${geistMono.variable} antialiased`}
+          >
+            <NuqsAdapter>
+              {children}
+            </NuqsAdapter>
+            <Toaster />
+          </body>
+        </html>
       </TRPCReactProvider>
     </ClerkProvider>
   );
