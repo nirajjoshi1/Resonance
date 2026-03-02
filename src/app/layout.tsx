6de90848a6ef70fd24d87c031c313f6ac95f11db
nirajjoshi1 <infocodewithniraj@gmail.com>
2026-03-02 23:04:47 +0545

    erro fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCReactProvider } from "@/trpc/client";
-import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,7 +27,6 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -35,12 +35,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <TRPCReactProvider>
-      <html lang="en">
-        <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
-          {children}
-          <Toaster />
-        </body>
-      </html>
+        <html lang="en">
+          <body
+            className={`${inter.variable} ${geistMono.variable} antialiased`}
+          >
+            <NuqsAdapter>
+              {children}
+            </NuqsAdapter>
+            <Toaster />
+          </body>
+        </html>
       </TRPCReactProvider>
     </ClerkProvider>
   );
